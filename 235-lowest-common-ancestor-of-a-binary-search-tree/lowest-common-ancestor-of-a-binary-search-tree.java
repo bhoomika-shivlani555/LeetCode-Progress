2804a652498f715fc30f0e3a9d504abb131f0fa9
bhoomika-shivlani555 <bhoomikashivlani@gmail.com>
2026-08-13 11:58:01 +0530

    Time: 7 ms (2.96%) | Memory: 47.9 MB (16.72%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,21 +9,34 @@
  */
 
 class Solution {
+    public boolean parents(TreeNode root,TreeNode p,ArrayList<TreeNode> list){
+       if(root==null)
+            return false;
+        list.add(root);
+        if(root.val==p.val)
+            return true;
+
+        if(parents(root.left,p,list) || parents(root.right,p,list))
+            return true;
+        list.remove(list.size()-1);
+        return false;
+    }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null)
-            return null;
-        if(root==p)
-            return p;
-        if(root==q)
+        ArrayList<TreeNode> list1=new ArrayList<>();
+        ArrayList<TreeNode> list2=new ArrayList<>();
+
+        parents(root,p,list1);
+        parents(root,q,list2);
+
+        int x=0;
+        for(x=0;x<list1.size() && x<list2.size();x++)
+        {
+            if(list1.get(x).val!=list2.get(x).val)
+                return list1.get(x-1);
+        }
+        if(x<list1.size())
             return q;
-        TreeNode l=lowestCommonAncestor(root.left,p,q);
-        TreeNode r=lowestCommonAncestor(root.right,p,q);
-
-        if(l==null)
-            return r;
-        if(r==null)
-            return l;
-
-        return root;
+        else
+            return p;
     }
 }
