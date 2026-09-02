b12af08f6ca3858a6358caf6ae7245e74c6d9ade
bhoomika-shivlani555 <bhoomikashivlani@gmail.com>
2026-09-02 14:46:56 +0530

    Time: 2 ms (36.32%) | Memory: 46.4 MB (36.44%) - LeetSync

---
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.java
@@ -0,0 +1,54 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        Queue<Node> q=new LinkedList<>();
+        if(root==null)
+            return root;
+        q.offer(root);
+        q.offer(null);
+
+        while(!q.isEmpty())
+        {
+            Node curr=q.poll();
+            // if(curr!=null)
+            //     curr.next=q.peek();
+            if(curr!=null)
+            {
+                curr.next=q.peek();
+                if(curr.left!=null)
+                    q.offer(curr.left);
+                if(curr.right!=null)
+                    q.offer(curr.right);
+            }
+            else
+            {
+                if(q.isEmpty())
+                    break;
+                q.offer(null);
+            }
+        }
+        return root;
+    }
+}
