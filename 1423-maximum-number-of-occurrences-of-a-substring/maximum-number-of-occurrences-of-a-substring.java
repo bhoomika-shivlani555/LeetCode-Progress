964fd7daec071b4814ca535538db260f3c335249
bhoomika-shivlani555 <bhoomikashivlani@gmail.com>
2026-09-14 16:11:05 +0530

    Time: 142 ms (14.96%) | Memory: 50.2 MB (14.46%) - LeetSync

---
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
+        HashMap<Character,Integer> freq=new HashMap<>();
+        HashMap<String,Integer> map=new HashMap<>();
+        String str="";
+        int l=0,r=0;
+        while(r<s.length())
+        {
+            char c=s.charAt(r);
+            str=str+c;        //character add str andin map too
+            if(freq.containsKey(c))
+                freq.put(c,freq.get(c)+1);
+            else
+                freq.put(c,1);
+
+            if(r-l+1>minSize)
+            {
+                char c1=s.charAt(l);
+                str=str.substring(1,str.length());
+                l++;
+                freq.put(c1,freq.get(c1)-1);
+                if(freq.get(c1)==0)
+                    freq.remove(c1);
+            }
+            
+            if(r-l+1==minSize)
+            {
+                //condition
+                if(freq.size()<=maxLetters)         //unique char<= maxletters
+                {
+                    if(map.containsKey(str))
+                        map.put(str,map.get(str)+1);
+                    else
+                        map.put(str,1);
+                }
+            }
+            r++;
+        }
+        int max=0;
+        for(String x:map.keySet())
+        {
+            System.out.println(x);
+            max=Math.max(max,map.get(x));
+        }
+        return max;
+    }
+}
