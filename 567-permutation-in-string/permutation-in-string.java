8e3b235d6697b70a731f5b6c9bac2cb498a52614
bhoomika-shivlani555 <bhoomikashivlani@gmail.com>
2026-09-17 18:40:32 +0530

    Time: 6 ms (87.36%) | Memory: 43.9 MB (73.80%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -3,26 +3,27 @@ class Solution {
         int n=s2.length();
         int k=s1.length();
         int l=0,r=0;
-        HashMap<Character,Integer> map=new HashMap<>();
-        HashMap<Character,Integer> map2=new HashMap<>();
+        int arr[]=new int[26];
+        int arr2[]=new int[26];
+
+        Arrays.fill(arr,0);
+        Arrays.fill(arr2,0);
         for(int x=0;x<k;x++)
-            map.put(s1.charAt(x),map.getOrDefault(s1.charAt(x),0)+1);
+            arr[s1.charAt(x)-'a']++;
 
         while(r<n)
         {
             char c=s2.charAt(r);
-            map2.put(c,map2.getOrDefault(c,0)+1);
+            arr2[c-'a']++;
             if(r-l+1>k)
             {
                 char c1=s2.charAt(l);
-                map2.put(c1,map2.get(c1)-1);
-                if(map2.get(c1)==0)
-                    map2.remove(c1);
+                arr2[c1-'a']--;
                 l++;
             }
             if(r-l+1==k)
             {
-                if(map2.equals(map))
+                if(Arrays.equals(arr,arr2))
                     return true;
             }
             r++;
